4b766eb9de49fee5c12cf09e49870734dfcd9874
Juju <julia.almeida3@estudante.ifms.edu.br>
2025-11-24 20:52:20 -0400

    3

--- app.js
+++ app.js
@@ -20,6 +20,26 @@ app.get("/tico", (req, res) => {
   res.send("teco");
 });
 
+app.get("/pokemons", (req, res) => {
+  const pokemons = [
+    "Pikachu não conta como captura por ser o inicial",
+    "Caterpie",
+    "Pidgeotto",
+    "Bulbasaur",
+    "Charmander",
+    "Squirtle",
+    "Krabby",
+    "Primeape",
+    "Muk",
+    "Tauros",
+    "Lapras"
+  ];
+
+  res.json(pokemons);
+});
+
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
