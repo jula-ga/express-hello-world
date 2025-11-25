528bd81192bdd41d584d795c01218d0f47a59d9f
Juju <julia.almeida3@estudante.ifms.edu.br>
2025-11-24 20:50:51 -0400

    1

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ app.get('/req', (req, res) => {
     res.send('Yo!')
 })
 
+app.get("/meunome", (req, res) => {
+  res.send("Meu nome é Arthur Redigolo");
+});
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
